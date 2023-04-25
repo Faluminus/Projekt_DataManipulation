bd13d31f4212fc4db829905fad3149cf075419b5
school <semradfilip@email.cz>
2023-04-25 21:19:52 +0200

    k

---
+++ ConsoleApp1/DataManipulation .cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class DataManipulation
+    {
+        public void Add()
+        {
+
+        }
+        public void Remove()
+        {
+
+        }
+        public void Update()
+        {
+
+        }
+        public void Show_Data()
+        {
+
+        }
+        public void Sort_Data()
+        {
+
+        }
+        public void Create(string type)
+        {
+            string FilePath = "C:\\Users\\ja\\source\\repos\\ConsoleApp1\\ConsoleApp1\\DataStorage\\";
+            switch(type)
+            {
+                case "-c":
+                    FileStream fs = File.Create(FilePath);
+                    break;
+                case "-x":
+                    
+                    break;
+                case "-j":
+
+                    break;
+                default:
+                    Console.WriteLine($"Uknown type: {type}");
+                    break;
+
+            }
+        }
+        public void Select()
+        {
+
+        }
+        public void Help()
+        {
+            Console.WriteLine(@"-------------------------------------------------------------------------------
+                    command: create
+                            -c   creates csv file
+                            -x   creates xml file
+                            -j   creates json file
+
+                    command: showdata 
+                            -l        lists all datafiles
+
+                    command: openfile [nameoffile]
+                            opens specified datafile
+                            
+                    ");   
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ConsoleApp1
 {
@@ -10,6 +16,82 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            DataManipulation dataManipulation = new DataManipulation();
+            string file = "";
+            if (Directory.GetFiles("C:\\ConsoleApp1\\ConsoleApp1\\DataStorage\\").Count() == 0)
+            {
+                Console.WriteLine("Reminder!!!! the data file must be inserted into DataStorage folder or created by proper command (do --help for getting all cmds) !!!");
+            }
+            while (true)
+            {
+                Text_Color("blue", $"User/{file}:",false);
+                string input = Console.ReadLine();
+                switch(input.ToLower())
+                {
+                    case "--help":
+                        dataManipulation.Help();
+                        break;
+                    case "create":
+                        Text_Color("green", "Type/:",false);
+                        input = Console.ReadLine();
+                        dataManipulation.Create(input);
+                        
+                        break;
+                    case "cl":
+                        Console.Clear();
+                        break;
+                    default:
+                        Text_Color("red","Uknow command",true);
+                        break;
+                }
+            }
+        }
+        //text_color function colors text and writes to console
+        private static void Text_Color(string color, string text,bool withline )//"withline" for decision if you want writeline or just write
+        {
+            switch(color)
+            {
+                case "red":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    switch(withline)
+                    {
+                        case true:
+                            Console.WriteLine(text);
+                            break;
+                        case false:
+                            Console.Write(text);
+                            break;
+                    }
+                    Console.ResetColor();
+
+                    break;
+                case "green":
+                    Console.ForegroundColor= ConsoleColor.Green;
+                    switch (withline)
+                    {
+                        case true:
+                            Console.WriteLine(text);
+                            break;
+                        case false:
+                            Console.Write(text);
+                            break;
+                    }
+                    Console.ResetColor();
+                    break;
+                case "blue":
+                    Console.ForegroundColor= ConsoleColor.Blue;
+                    switch (withline)
+                    {
+                        case true:
+                            Console.WriteLine(text);
+                            break;
+                        case false:
+                            Console.Write(text);
+                            break;
+                    }
+                    Console.ResetColor ();
+                    break;
+            }
         }
     }
 }
